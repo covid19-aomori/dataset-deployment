58a72e4703fd535c541fe4b1ff881d0610c1cd96
youyo <1003ni2@gmail.com>
2020-03-28 16:02:43 +0900

    Call netlify build hook

--- function/netlify.py
+++ function/netlify.py
@@ -1,10 +1,28 @@
 from jeffy.framework import setup
+import boto3
+import os
+import requests
 
 
 app = setup()
+
+product_id = os.environ.get('ProductId')
+env = os.environ.get('Env')
 
 
 @app.decorator.auto_logging
 @app.decorator.sqs
 def deploy(event, context):
-    return event
+    ssm = boto3.client('ssm')
+    response = ssm.get_parameter(
+        Name=f'/{product_id}/{env}/netlify/build_hook_url',
+        WithDecryption=True
+    )
+    netlify_build_hook_url = response['Parameter']['Value']
+
+    response = requests.post(netlify_build_hook_url)
+
+    return {
+        'reason': response.reason,
+        'status_code': response.status_code
+    }
